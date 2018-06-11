756015bc295fbad8bb6a4ebf09a88020baa7f628
Charlie <cmainegra@protonmail.com>
2018-06-11 15:29:14 -0400

    melinda

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -22,3 +22,79 @@
 //      * When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
 
 //    * The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
+
+//two empty variables, 
+var current = 0;
+var goal = Math.floor(Math.random() * 87)+ 34;
+var card1 = Math.floor(Math.random() * 13) + 1;
+var card2 = Math.floor(Math.random() * 13) + 1;
+var card3 = Math.floor(Math.random() * 13) + 1;
+var card4 = Math.floor(Math.random() * 13) + 1;
+console.log(card1);
+console.log(goal);
+
+$(document).ready(function() {
+
+$(".goal").html(goal);
+
+$("#card1").on("click", function(){
+    current += card1;
+    $(".current").html(current);
+    console.log(current)
+    if (current === goal) {
+        alert("You Win!");
+    } else if (current > goal) {
+        alert("You Lose!");
+    }
+});
+$("#card2").on("click", function(){
+    current += card2;
+    $(".current").html(current);
+    console.log(current);
+    if (current === goal) {
+        alert("You Win!");
+    } else if (current > goal) {
+        alert("You Lose!");
+    }
+});
+$("#card3").on("click", function(){
+    current += card3;
+    $(".current").html(current);
+    console.log(current);
+    if (current === goal) {
+        alert("You Win!");
+    } else if (current > goal) {
+        alert("You Lose!");
+    }
+});
+$("#card4").on("click", function(){
+    current += card4;
+    $(".current").html(current);
+    console.log(current);
+    if (current === goal) {
+        alert("You Win!");
+    } else if (current > goal) {
+        alert("You Lose!");
+    }
+});
+
+
+
+
+
+
+});
+//goal 34-120
+    //current sum of values of clicks
+//generate random variables for game
+    //a value of 1-12 for each card
+    //four positions each have a different value; between 1-12 
+//user clicks picture
+    //add user value of click for that image to current
+//user clicks other picture
+    //add value of picture to current
+//if current > goal 
+    //user loses
+//if current === goal
+    //user wins
+
